632401118ea66407d0a7a68f3e5c45ed91aef98f
ioanabirsan <ioana.c.birsan@gmail.com>
2018-03-07 19:25:20 +0200

    Improve homework

--- lab3/1.sql
+++ lab3/1.sql
@@ -12,38 +12,39 @@ BEGIN
     for v_contor1 in 1..10000 loop
         v_valoare_curenta := v_contor1;
         v_suma_cifre := 0;
+        -- valori > 9
         if(v_valoare_curenta > 9) 
           then 
               -- calculez suma cifrelor
               while (v_valoare_curenta > 10) loop       
                  v_rest := mod(v_valoare_curenta, 10);
                  v_suma_cifre := v_suma_cifre + v_rest;
-                 v_valoare_curenta := v_valoare_curenta / 10;
+                 v_valoare_curenta := trunc(v_valoare_curenta / 10);
                  v_cat := v_valoare_curenta; 
               end loop;
               v_suma_cifre := v_suma_cifre + v_cat;
-              -- verific daca valoarea sumei cifrelor modulo 9 este egala cu valoarea 
-              -- unei constante declarate la inceputul scriptului
+              
+              -- daca valoarea sumei cifrelor modulo 9 este egala cu valoarea 
+              -- unei constante declarate la inceputul scriptului atunci verific primalitatea
               if(mod(v_suma_cifre, 9) = v_constanta)
                 then 
                     -- verific primalitatea
-                    if (v_contor1 <= 1)
-                      then DBMS_OUTPUT.PUT_LINE(v_contor1 || '   ' ||0);
-                      elsif (v_contor1 <= 3) then 
-                        DBMS_OUTPUT.PUT_LINE(v_contor1 || '   ' || 1);
-                      elsif ((mod(v_contor1, 2) = 0) or (mod(v_contor1, 3) = 0)) then 
+                    if (mod(v_contor1, 2) = 0) 
+                      then 
+                      -- nr este par -> nu este prim
                         DBMS_OUTPUT.PUT_LINE(v_contor1 || '   ' || 0);
                       else 
-                        v_index := 5;
+                        v_index := 3;
                         while ((v_index * v_index) <= v_contor1) loop
-                          if ((mod(v_contor1,v_index) = 0) or (mod(v_contor1, v_index + 2) = 0)) 
-                            then DBMS_OUTPUT.PUT_LINE(v_contor1 || '   ' || 0);
+                          if (mod(v_contor1, v_index) = 0) 
+                            then DBMS_OUTPUT.PUT_LINE(v_contor1 || '   ' || 0); exit;
+                            else DBMS_OUTPUT.PUT_LINE(v_contor1 || '   ' || 1); exit;
                           end if;
-                          v_index := v_index + 6;
+                          v_index := v_index + 2;
                         end loop;
-                        DBMS_OUTPUT.PUT_LINE(v_contor1 || '   ' || 1);
                     end if; 
               end if;
+            -- valori <= 9 
             else 
               if(mod(v_contor1, 9) = v_constanta)
                 then
@@ -53,5 +54,5 @@ BEGIN
                   end if;
               end if;
         end if;
-    end loop;
+    end loop;  
 END;
