75398f8e56069e12b8197f5c51dc23427193e05e
ioanabirsan <ioana.c.birsan@gmail.com>
2018-03-09 13:33:49 +0200

    Solve problem 2

--- lab3/1.sql
+++ lab3/1.sql
@@ -2,8 +2,8 @@ set serveroutput on;
 DROP TABLE tabel;
 CREATE TABLE tabel(A integer, B integer);
 DECLARE
-   v_constanta constant integer := 5;
-   v_contor1 tabel.A%type;
+   v_constanta constant integer := 7;
+   v_contor tabel.A%type;
    v_valoare_curenta tabel.A%type;
    v_cat tabel.A%type;
    v_rest tabel.A%type;
@@ -11,8 +11,8 @@ DECLARE
    v_index tabel.A%type;
    v_is_prime boolean;
 BEGIN
-    for v_contor1 in 1..10000 loop
-        v_valoare_curenta := v_contor1;
+    for v_contor in 1..10000 loop
+        v_valoare_curenta := v_contor;
         v_suma_cifre := 0;
         -- valori > 9
         if(v_valoare_curenta > 9) 
@@ -31,36 +31,36 @@ BEGIN
               if(mod(v_suma_cifre, 9) = v_constanta)
                 then 
                     -- verific primalitatea
-                    if (mod(v_contor1, 2) = 0) 
+                    if (mod(v_contor, 2) = 0) 
                       then 
                       -- nr este par -> nu este prim
-                        INSERT INTO tabel values (v_contor1, 0);
-                        DBMS_OUTPUT.PUT_LINE(v_contor1||'   '|| 0);
+                        INSERT INTO tabel values (v_contor, 0);
+                        DBMS_OUTPUT.PUT_LINE(v_contor||'   '|| 0);
                       else 
                         v_index := 3;
                         v_is_prime := true;
-                        while (((v_index * v_index) <= v_contor1) and v_is_prime) loop
-                          if (mod(v_contor1, v_index) = 0) 
-                            then INSERT INTO tabel values (v_contor1, 0);
-                                  DBMS_OUTPUT.PUT_LINE(v_contor1||'   '|| 0);
+                        while (((v_index * v_index) <= v_contor) and v_is_prime) loop
+                          if (mod(v_contor, v_index) = 0) 
+                            then INSERT INTO tabel values (v_contor, 0);
+                                  DBMS_OUTPUT.PUT_LINE(v_contor||'   '|| 0);
                                    v_is_prime := false;
                           end if;
                           v_index := v_index + 2;
                         end loop;
                         if (v_is_prime)
                           then
-                            INSERT INTO tabel values (v_contor1, 1); 
-                            DBMS_OUTPUT.PUT_LINE(v_contor1||'   '|| 1);
+                            INSERT INTO tabel values (v_contor, 1); 
+                            DBMS_OUTPUT.PUT_LINE(v_contor||'   '|| 1);
                         end if;
                     end if; 
               end if;
             -- valori <= 9 
             else 
-              if(mod(v_contor1, 9) = v_constanta)
+              if(mod(v_contor, 9) = v_constanta)
                 then
-                  if ((v_contor1 = 2) or (v_contor1 = 3) or (v_contor1 = 5) or (v_contor1 = 7))
-                      then INSERT INTO tabel values (v_contor1, 1); DBMS_OUTPUT.PUT_LINE(v_contor1||'   '|| 1);
-                      else INSERT INTO tabel values (v_contor1, 0); DBMS_OUTPUT.PUT_LINE(v_contor1||'   '|| 0);
+                  if ((v_contor = 2) or (v_contor = 3) or (v_contor = 5) or (v_contor = 7))
+                      then INSERT INTO tabel values (v_contor, 1); DBMS_OUTPUT.PUT_LINE(v_contor||'   '|| 1);
+                      else INSERT INTO tabel values (v_contor, 0); DBMS_OUTPUT.PUT_LINE(v_contor||'   '|| 0);
                   end if;
               end if;
         end if;
--- lab3/2.sql
+++ lab3/2.sql
@@ -1,20 +1,63 @@
 /*
-Utilizand un cursor explicit deschis cu clauza FOR UPDATE, construiti un script care sa faca update campului B din tabelul de la punctul 1. 
+Utilizand un cursor explicit deschis cu clauza FOR UPDATE, construiti un script care sa faca update campului B din 
+tabelul de la punctul 1. 
 In loc de prim acesta va indica daca numarul respectiv este sau nu o valoare din sirul lui Fibonacci. 
 Scriptul va afisa numarul de valori updatate (adica ce s-a schimbat din 0 in 1 + ce s-a schimbat din 1 in 0). 
 */
 
-declare
+set serveroutput on;
+DECLARE
   cursor informatii is select * from tabel order by tabel.A;
+  cursor update_informatii is select * from tabel for update of B nowait;
    v_info_a tabel.A%type;
    v_info_b tabel.B%type;
-begin
+   v_numar_valori_update tabel.A%type := 0;
+   v_fib_condition1 tabel.A%type;
+   v_fib_condition2 tabel.A%type;
+   v_radacina_patrata1 tabel.A%type;
+   v_radacina_patrata2 tabel.A%type;
+BEGIN
+    -- afisare tabel initial
     open informatii;
-    DBMS_OUTPUT.PUT_LINE('Informatii stocate in tabel');
+    DBMS_OUTPUT.PUT_LINE('Informatiile stocate initial in tabel');
+    
     loop
         fetch informatii into v_info_a, v_info_b;
         exit when informatii%NOTFOUND;
         DBMS_OUTPUT.PUT_LINE(v_info_a||'   '|| v_info_b);
     end loop;
     close informatii; 
-end;
+    
+    DBMS_OUTPUT.PUT_LINE('----------------------------------');
+    
+    -- prelucrare si update tabel
+    open update_informatii;
+    DBMS_OUTPUT.PUT_LINE('Informatiile din tabel dupa update');
+    loop
+        fetch update_informatii into v_info_a, v_info_b;
+        v_fib_condition1 := 5 * v_info_a * v_info_a + 4;
+        v_fib_condition2 := 5 * v_info_a * v_info_a - 4;
+        v_radacina_patrata1 := sqrt(v_fib_condition1);
+        v_radacina_patrata2 := sqrt(v_fib_condition2);
+        
+        if((v_radacina_patrata1 * v_radacina_patrata1 = v_fib_condition1) or (v_radacina_patrata2 * v_radacina_patrata2 = v_fib_condition2))
+          then 
+              if (v_info_b = 0) 
+                then UPDATE tabel SET B = 1;
+                v_numar_valori_update := v_numar_valori_update + 1;
+              end if;
+          else 
+              if(v_info_b = 1)
+                then UPDATE tabel SET B = 0;
+                v_numar_valori_update := v_numar_valori_update + 1;
+              end if;
+       end if;
+      exit when update_informatii%NOTFOUND;
+      DBMS_OUTPUT.PUT_LINE(v_info_a||'   '|| v_info_b);
+    end loop;
+    close update_informatii;
+         
+    DBMS_OUTPUT.PUT_LINE('----------------------------------');
+    
+    DBMS_OUTPUT.PUT_LINE('Numar valori updatate: '|| v_numar_valori_update);
+END;
