70c039053fb834e5da7b8a2f81f9de6ed0cb93cc
ioanabirsan <ioana.c.birsan@gmail.com>
2018-03-21 12:45:24 +0200

    Add first ex from lab4

---
+++ lab4/1.sql
@@ -0,0 +1,12 @@
+/* 
+  1. O procedura (privata) ce returneaza varsta unui student 
+  (in ani, luni si zile - vezi tema nr.1) a acelui student;
+*/
+CREATE OR REPLACE PROCEDURE varsta_student (p_data_nastere IN studenti.data_nastere%type, p_numar_ani OUT integer, p_numar_luni OUT integer, p_numar_zile OUT integer) AS
+    v_data_intermediara varchar2(30);
+BEGIN
+    p_numar_ani := floor(months_between(sysdate, p_data_nastere) /12); 
+    p_numar_luni := trunc(months_between(sysdate, p_data_nastere));
+    v_data_intermediara := add_months(p_data_nastere, p_numar_luni);
+    p_numar_zile := trunc(sysdate - to_date(v_data_intermediara));
+END;
---
+++ lab4/test.sql
@@ -0,0 +1,21 @@
+/* Test exercitiul 1 lab 3 */
+
+set serveroutput on;
+DECLARE
+      cursor varste_studenti is select nume, prenume, data_nastere from studenti order by nume asc, prenume asc;
+      v_nume studenti.nume%type;
+      v_prenume studenti.prenume%type;
+      v_data_nastere studenti.data_nastere%type;
+      v_numar_zile int := 0;
+      v_numar_luni int := 0;
+      v_numar_ani int := 0;
+BEGIN
+    open varste_studenti;
+    loop
+        fetch varste_studenti into v_nume, v_prenume, v_data_nastere;
+        exit when varste_studenti%NOTFOUND;
+        varsta_student(v_data_nastere, v_numar_ani, v_numar_luni, v_numar_zile);
+        DBMS_OUTPUT.PUT_LINE(v_nume || ' ' || v_prenume || ' ' || v_data_nastere || ' => '  || v_numar_ani || ' ani, ' || v_numar_luni || ' luni, ' || v_numar_zile || ' zile.');
+    end loop;
+    close varste_studenti; 
+END;
